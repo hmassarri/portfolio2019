d98798426f67b819a8a2d744f7dc476781c0eff9
Hugo Massarri <hmassarri@PRNNVENKATA.alpha.eduh.co>
2019-12-27 11:00:47 -0500

    added code to make active class in menu

--- includes/banner_drawer.php
+++ includes/banner_drawer.php
@@ -12,24 +12,29 @@
           fluid responsive design, HTML5 & CSS3/SASS, JavaScript & jQuery, PHP</p>
       </div>
     </div>
+
+
+    <!-- Active Page -->
+    <?php
+      function pageName() {
+        return substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+      }
+      $currentPage = pageName();
+      //echo $currentPage;
+    ?>
+
     <!-- Tabs -->
     <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-layout--large-screen-only" style="background-color: #000000 !important;">
-      <a href="#fixed-tab-1" class="mdl-layout__tab is-active">Home</a>
-      <a href="<?php echo $root.'work.php'; ?>" class="mdl-layout__tab">Work</a>
+      <a href="<?php echo $root.'index.php'; ?>" class="mdl-layout__tab <?php echo $currentPage == 'index.php' ? 'is-active':NULL ?>">Home</a>
+      <a href="<?php echo $root.'work.php'; ?>" class="mdl-layout__tab <?php
+        if ($currentPage == 'work.php' || $currentPage == 'pykescorner.php') {
+          echo 'is-active';
+        }
+        else { echo NULL; } ?>">Work</a>
       <a href="#fixed-tab-3" class="mdl-layout__tab">Process</a>
       <a href="#fixed-tab-4" class="mdl-layout__tab">Blog</a>
       <a href="#fixed-tab-5" class="mdl-layout__tab">Contact</a>
     </div>
-
-
-
-    <!-- PARSING URL WITH PHP -->
-    <?php
-      $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-      $escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
-      echo '<a href="' . $escaped_url . '">' . $escaped_url . '</a>';
-    ?>
-
 
   </header>
 
