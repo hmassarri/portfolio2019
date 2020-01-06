96d7d371ee8afaa96bf55f1ea0c0600d4ce04586
Hugo Massarri <hmassarri@PRNNVENKATA.alpha.eduh.co>
2020-01-06 14:55:23 -0500

    Reformatted all process images

--- content/content-process.php
+++ content/content-process.php
@@ -23,8 +23,8 @@
 
           <!-- Mental Model -->
           <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-            <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive" />
-            <div class="center-text my-4">
+            <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive mb-4" />
+            <div class="center-text">
               <h3>Mental Model</h3>
               <p>Capture user behavior, pain points, motivations, and needs from contextual inquiries. Group into logical pillars. Map out product features for each pillar to identify gaps, opportunities, and feature bloat.</p>
             </div>
@@ -33,14 +33,55 @@
           <!-- Personas -->
           <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
             <img src="img/process/2-SmashingMag-personas.jpg" class="img-responsive" />
-            <div class="center-text my-4">
+            <div class="center-text">
               <h3>Personas</h3>
               <p>Summarize the research data from your mental model into personas. This will help your team keep the user’s motivations, frustrrations, and goals in mind when evaluate design solutions.</p>
             </div>
           </div>
 
+          <!-- Customer Journey Map -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/3-Appcues-customerjourneymap.jpg" class="img-responsive mb-5" />
+            <div class="center-text">
+              <h3>Customer Journey Map</h3>
+              <p>After you have identified who will be using your product, write down the business goals/desired outcome for each persona. You then go back and fill in how they will discover your product, learn what it is all about, and how they will start using it.</p>
+            </div>
+          </div>
+
       </div>
       <!--/end grid -->
+
+      <div class="mdl-grid">
+
+        <!-- Mental Model -->
+        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+          <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive mb-4" />
+          <div class="center-text">
+            <h3>Mental Model</h3>
+            <p>Capture user behavior, pain points, motivations, and needs from contextual inquiries. Group into logical pillars. Map out product features for each pillar to identify gaps, opportunities, and feature bloat.</p>
+          </div>
+        </div>
+
+        <!-- Personas -->
+        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+          <img src="img/process/2-SmashingMag-personas.jpg" class="img-responsive" />
+          <div class="center-text">
+            <h3>Personas</h3>
+            <p>Summarize the research data from your mental model into personas. This will help your team keep the user’s motivations, frustrrations, and goals in mind when evaluate design solutions.</p>
+          </div>
+        </div>
+
+        <!-- Customer Journey Map -->
+        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+          <img src="img/process/3-Appcues-customerjourneymap.jpg" class="img-responsive mb-5" />
+          <div class="center-text">
+            <h3>Customer Journey Map</h3>
+            <p>After you have identified who will be using your product, write down the business goals/desired outcome for each persona. You then go back and fill in how they will discover your product, learn what it is all about, and how they will start using it.</p>
+          </div>
+        </div>
+
+    </div>
+    <!--/end grid -->
 
 
 
