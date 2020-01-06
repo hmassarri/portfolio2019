a7bb0fb1bf1900f337d4472c7404ddcd1dc4f5d9
Hugo Massarri <hmassarri@PRNNVENKATA.alpha.eduh.co>
2020-01-06 15:43:03 -0500

    process page filled in

--- content/content-process.php
+++ content/content-process.php
@@ -23,67 +23,138 @@
 
           <!-- Mental Model -->
           <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-            <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive mb-4" />
+            <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive" />
             <div class="center-text">
-              <h3>Mental Model</h3>
+              <h4>Mental Model</h4>
               <p>Capture user behavior, pain points, motivations, and needs from contextual inquiries. Group into logical pillars. Map out product features for each pillar to identify gaps, opportunities, and feature bloat.</p>
             </div>
           </div>
 
           <!-- Personas -->
           <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-            <img src="img/process/2-SmashingMag-personas.jpg" class="img-responsive" />
+            <img src="img/process/2-SmashingMag-personas.png" class="img-responsive" />
             <div class="center-text">
-              <h3>Personas</h3>
+              <h4>Personas</h4>
               <p>Summarize the research data from your mental model into personas. This will help your team keep the user’s motivations, frustrrations, and goals in mind when evaluate design solutions.</p>
             </div>
           </div>
 
           <!-- Customer Journey Map -->
           <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-            <img src="img/process/3-Appcues-customerjourneymap.jpg" class="img-responsive mb-5" />
+            <img src="img/process/3-Appcues-customerjourneymap.png" class="img-responsive" />
             <div class="center-text">
-              <h3>Customer Journey Map</h3>
+              <h4>Customer Journey Map</h4>
               <p>After you have identified who will be using your product, write down the business goals/desired outcome for each persona. You then go back and fill in how they will discover your product, learn what it is all about, and how they will start using it.</p>
             </div>
           </div>
 
-      </div>
-      <!--/end grid -->
+        </div>
+        <!--/end grid -->
 
-      <div class="mdl-grid">
-
-        <!-- Mental Model -->
-        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-          <img src="img/process/1-DanielEizans-mentalmodel.png" class="img-responsive mb-4" />
-          <div class="center-text">
-            <h3>Mental Model</h3>
-            <p>Capture user behavior, pain points, motivations, and needs from contextual inquiries. Group into logical pillars. Map out product features for each pillar to identify gaps, opportunities, and feature bloat.</p>
-          </div>
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
         </div>
 
-        <!-- Personas -->
-        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-          <img src="img/process/2-SmashingMag-personas.jpg" class="img-responsive" />
-          <div class="center-text">
-            <h3>Personas</h3>
-            <p>Summarize the research data from your mental model into personas. This will help your team keep the user’s motivations, frustrrations, and goals in mind when evaluate design solutions.</p>
+        <div class="mdl-grid">
+
+          <!-- Task Analysis -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/4-UXPlanet-taskanalysis.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Task Analysis</h4>
+              <p>Identifies user’s thought process and desired reactions toward each task flow segment.</p>
+            </div>
           </div>
+
+          <!-- Red Route Analysis -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/5-Medium-redroute.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Red Route Analysis</h4>
+              <p>Prioritize user experience for common tasks. Tasks used by few and rarely can be harder for user to access so that tasks used frequently by most people can be easily accomplished.</p>
+            </div>
+          </div>
+
+          <!-- Storyboards -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/6-NNGroup-storyboards.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Storyboards</h4>
+              <p>Visually depict scenarios with storyboards. The storyboard should show the context, the problem, the user’s fears/motiviations, and how the problem will be solved.</p>
+            </div>
+          </div>
+
+        </div>
+        <!--/end grid -->
+
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
         </div>
 
-        <!-- Customer Journey Map -->
-        <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
-          <img src="img/process/3-Appcues-customerjourneymap.jpg" class="img-responsive mb-5" />
-          <div class="center-text">
-            <h3>Customer Journey Map</h3>
-            <p>After you have identified who will be using your product, write down the business goals/desired outcome for each persona. You then go back and fill in how they will discover your product, learn what it is all about, and how they will start using it.</p>
+        <div class="mdl-grid">
+
+          <!-- Wireframes -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/7-HM-recordpanel.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Wireframes</h4>
+              <p>Visually communicate abstract concepts with users and team members to generate feedback. These can be drawn by hand or with software like Sketch, Balsamiq, Axure, or Omnigraffle</p>
+            </div>
           </div>
+
+          <!-- Rapid Prototyping -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/8-UXPin-rapidproto.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Rapid Prototyping</h4>
+              <p>Get users involved early in the design process with quick and inexpensive paper prototypes. Uncover usability issues and validate design decisions based on acutal user reactions.</p>
+            </div>
+          </div>
+
+          <!-- Style Tiles -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/9-HM-styletiles.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Style Tiles</h4>
+              <p>Jumpstart the visual design language even before the interaction designs are finalized. Start with common UI elements or components that would be found in the product your are building and iterate through several varations</p>
+            </div>
+          </div>
+
+        </div>
+        <!--/end grid -->
+
+        <div class="mdl-grid">
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
+          <div class="mdl-cell mdl-cell--12-col">&nbsp;</div>
         </div>
 
-    </div>
-    <!--/end grid -->
+        <div class="mdl-grid">
 
+          <!-- HiFidelity Mockups and Prototypes -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/10-HM-hifi.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>HiFidelity Mockups and Prototypes</h4>
+              <p>Apply product branding, grid structure, typography, color scheme, and icons. Develop interactive prototypes with tools like Sketch or Invision. This lets the team easily observe exactly how well the product will be received before the product gets built. It gives the team a chance to see and take time to identify any roadblocks that haven’t been surfaced yet.</p>
+            </div>
+          </div>
 
+          <!-- Pattern Library -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">
+            <img src="img/process/11-HM-patternlib.png" class="img-responsive" />
+            <div class="center-text">
+              <h4>Pattern Library</h4>
+              <p>Create a reusable pattern library in an app like Sketch so that you and your team members do not have to keep recreating commonly used controls over and over again in your designs.</p>
+            </div>
+          </div>
+
+          <!-- Style Tiles -->
+          <div class="mdl-cell mdl-cell--12-col-phone mdl-cell--12-col-tablet mdl-cell--4-col-desktop">&nbsp;</div>
+
+        </div>
+        <!--/end grid -->
 
       </div>
       <!--/page-content -->
